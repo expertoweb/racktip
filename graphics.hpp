159e9dab55215f649b77436475f7e51f79d0d138
expertoweb <expertoweb@gmail.com>
2014-10-03 13:16:47 +0200

    All coordinates

--- graphics.hpp
+++
@@ -1,7 +0,0 @@
-#include <cmath>
-#include <SDL/SDL.h>
-#include <SDL/SDL_image.h>
-
-void set_pixel(SDL_Surface *surface, int x, int y, Uint8 pixel);
-void draw_circle(SDL_Surface *surface, int n_cx, int n_cy, int radius, Uint8 pixel);
-void fill_circle(SDL_Surface *surface, int cx, int cy, int radius, Uint8 pixel);
